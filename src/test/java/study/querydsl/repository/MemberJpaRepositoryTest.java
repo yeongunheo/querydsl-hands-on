b90c098fdd81e7d17d0a67a4bcc2be296233f1c2
yeongun <gjduddns@gmail.com>
2023-02-27 00:16:45 +0900

    [섹션5. 실무 활용 - 순수 JPA와 Querydsl] 순수 JPA 리포지토리와 Querydsl

---
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,42 @@
+package study.querydsl.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void basicTest() {
+        // given
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        // when
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+        assertThat(result1).containsExactly(member);
+        assertThat(result2).containsExactly(member);
+    }
+
+}
