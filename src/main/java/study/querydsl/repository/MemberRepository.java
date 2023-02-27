28196cffd1a6021643c93bd71c36b1cd5b5c6a0d
yeongun <gjduddns@gmail.com>
2023-02-27 10:12:57 +0900

    [섹션6. 스프링 데이터 JPA가 제공하는 Querydsl 기능] 인터페이스 지원 - QuerydslPredicateExecuter

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     //SELECT m FROM Member m WHERE m.username = ?
     List<Member> findByUsername(String username);
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.querydsl.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -16,6 +18,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -27,25 +30,8 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    public void basicTest() {
-        // given
-        Member member = new Member("member1", 10);
-        memberRepository.save(member);
-
-        // when
-        Member findMember = memberRepository.findById(member.getId()).get();
-        List<Member> result1 = memberRepository.findAll();
-        List<Member> result2 = memberRepository.findByUsername("member1");
-
-        // then
-        assertThat(findMember).isEqualTo(member);
-        assertThat(result1).containsExactly(member);
-        assertThat(result2).containsExactly(member);
-    }
-
-    @Test
-    public void searchTest() {
+    @BeforeEach
+    void setup() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -60,7 +46,27 @@ class MemberRepositoryTest {
         em.persist(member2);
         em.persist(member3);
         em.persist(member4);
+    }
 
+    @Test
+    public void basicTest() {
+        // given
+        Member member = new Member("member10", 10);
+        memberRepository.save(member);
+
+        // when
+        Member findMember = memberRepository.findById(member.getId()).get();
+        List<Member> result1 = memberRepository.findAll();
+        List<Member> result2 = memberRepository.findByUsername("member10");
+
+        // then
+        assertThat(findMember).isEqualTo(member);
+        assertThat(result1).containsExactly(member);
+        assertThat(result2).containsExactly(member);
+    }
+
+    @Test
+    public void searchTest() {
         MemberSearchCondition condition = new MemberSearchCondition();
         condition.setAgeGoe(35);
         condition.setAgeLoe(40);
@@ -73,21 +79,6 @@ class MemberRepositoryTest {
 
     @Test
     public void searchPageSimpleTest() {
-        Team teamA = new Team("teamA");
-        Team teamB = new Team("teamB");
-        em.persist(teamA);
-        em.persist(teamB);
-
-        Member member1 = new Member("member1", 10, teamA);
-        Member member2 = new Member("member2", 20, teamA);
-
-        Member member3 = new Member("member3", 30, teamB);
-        Member member4 = new Member("member4", 40, teamB);
-        em.persist(member1);
-        em.persist(member2);
-        em.persist(member3);
-        em.persist(member4);
-
         MemberSearchCondition condition = new MemberSearchCondition();
         PageRequest pageRequest = PageRequest.of(0, 3);
 
@@ -95,5 +86,14 @@ class MemberRepositoryTest {
 
         assertThat(result).hasSize(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40)
+                .and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
